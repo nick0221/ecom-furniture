f9a2930cceac17cf7df901e60471aed1cb260c51
NickDevs <nick022190@gmail.com>
2026-05-30 06:50:45 +0800

    feat: change currency from USD to Philippine Peso (PHP)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatPrice(price: number): string {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-PH", {
     style: "currency",
-    currency: "USD",
+    currency: "PHP",
   }).format(price);
 }
 
